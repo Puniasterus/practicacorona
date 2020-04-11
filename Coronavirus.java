8a519bbea084d0339432a8b87a09c52f27428045
Puniasterus <puniasterus@gmail.com>
2020-04-11 12:25:56 +0200

    Update Coronavirus.java
    Extres

--- Coronavirus.java
+++ Coronavirus.java
@@ -1,9 +1,44 @@
 public class Coronavirus{
-private float infeccio;
-private float mortaldat;
-Coronavirus(float x, float y)
-{
-infeccio=xx;
-mortaldat=yy;
-}
+
+    public String Cadena()
+    {
+    return "Infectats: " + infeccio + " Mortaldat: " + mortaldat;
+    }
+    private float infeccio;
+    private float mortaldat;
+    private boolean toString;
+    
+    Coronavirus(float x, float y)
+        {
+        infeccio = x;
+        mortaldat = y;
+        }
+    
+    public Coronavirus(){
+        this.infeccio = 0;
+        this.mortaldat = 0;
+    }
+    
+    public void setInfeccio(float infeccio) {
+        this.infeccio = infeccio;
+    }
+
+    public void setMortaldat(float mortaldat) {
+        this.mortaldat = mortaldat;
+    }
+
+    public float getInfeccio() {
+        return infeccio;
+    }
+
+    public float getMortaldat() {
+        return mortaldat;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    public void start(){
+    System.out.println(Cadena());
+    }
 }
